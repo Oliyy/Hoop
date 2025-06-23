98a0ace9e7648623ff59a7b66b837e94fbd488d2
Oliyy <oliy@oliy.org>
2025-06-23 11:54:01 +0900

    Initial Commit

---
+++ Hoop/HoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoopApp.swift
+//  Hoop
+//
+//  Created by Oliy Barrett on 6/23/25.
+//
+
+import SwiftUI
+
+@main
+struct HoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HoopTests/HoopTests.swift
@@ -0,0 +1,17 @@
+//
+//  HoopTests.swift
+//  HoopTests
+//
+//  Created by Oliy Barrett on 6/23/25.
+//
+
+import Testing
+@testable import Hoop
+
+struct HoopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
